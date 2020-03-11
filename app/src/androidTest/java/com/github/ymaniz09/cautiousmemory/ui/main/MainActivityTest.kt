91409c54519d1b9ddcdab19a9e8b1f2fc43d65e8
Ygor <ygor.maniz@gmail.com>
2020-03-11 16:14:12 -0300

    Activity navigation testing

--- app/src/androidTest/java/com/github/ymaniz09/cautiousmemory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/ymaniz09/cautiousmemory/ui/main/MainActivityTest.kt
@@ -2,6 +2,9 @@ package com.github.ymaniz09.cautiousmemory.ui.main
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -39,5 +42,49 @@ class MainActivityTest {
 
         onView(withId(R.id.activity_main_title))
             .check(matches(withText(R.string.text_main_activity)))
+    }
+
+    @Test
+    fun test_navSecondaryActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity))
+            .perform(click())
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_toMainActivity_using_BackButton() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity))
+            .perform(click())
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back))
+            .perform(ViewActions.click())
+
+        onView(withId(R.id.main))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_toMainActivity_using_AndroidBackButton() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity))
+            .perform(click())
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+
+        pressBack()
+
+        onView(withId(R.id.main))
+            .check(matches(isDisplayed()))
     }
 }
